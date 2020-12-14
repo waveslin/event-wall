fb7717fe3189ee8c24be0f8aee556d00e5da30c8
waves <35038769+waveslin@users.noreply.github.com>
2020-12-14 13:02:04 -0500

    Update FbConfig.js

--- src/config/FbConfig.js
+++ src/config/FbConfig.js
@@ -4,14 +4,7 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDsvJRY34o4y2GttwZb9aoUYA48Eqv8jds",
-    authDomain: "events-wall.firebaseapp.com",
-    databaseURL: "https://events-wall.firebaseio.com",
-    projectId: "events-wall",
-    storageBucket: "events-wall.appspot.com",
-    messagingSenderId: "420764841323",
-    appId: "1:420764841323:web:d9e496b04520b9aa3d082e",
-    measurementId: "G-DM1M3124Y7"
+    
 };
  // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
